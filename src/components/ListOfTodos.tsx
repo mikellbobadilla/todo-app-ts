6542362ab4e7c25c9ddf5478ca15e03174e234b0
Sting <bobadillasting@gmail.com>
2024-03-10 02:11:21 -0300

    Add NavLink and useTodoAction to ListOfTodos component

--- src/components/ListOfTodos.tsx
+++ src/components/ListOfTodos.tsx
@@ -1,6 +1,6 @@
+import { NavLink } from 'react-router-dom'
 import { useAppSelector } from '../hooks/store'
-import { CheckIcon } from './icons/CheckIcon'
-import { CloseIcon } from './icons/CloseIcon'
+import { useTodoAction } from '../hooks/useTodoAction'
 import { EditIcon } from './icons/EditIcons'
 import { TrashIcon } from './icons/TrashIcon'
 
@@ -8,30 +8,45 @@ export function ListOfTodos() {
     // Get the todos from the store
     const todos = useAppSelector(state => state.todos)
 
+    const { removeTodo, setIsDone } = useTodoAction()
+
     return (
         <section>
-            <h1 className='text-3xl text-center mt-3 text-cyan-500 font-bold'>
-                List of Todos
-            </h1>
+            <div className='flex justify-between items-center py-2'>
+                <h2 className='text-2xl text-sky-500 font-semibold'>
+                    List of Todos
+                </h2>
+                <p>
+                    Todos <span className='bg-blue-200 ml-2 border text-blue-800 border-blue-600 px-4 inline-block rounded-2xl text-sm'>{todos.length}</span>
+                </p>
+            </div>
 
-            <ul className='grid grid-cols-1 gap-2 p-3 mt-3'>
+            <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-3'>
                 {todos.map(todo => (
-                    <li key={todo.id} className={`flex flex-col border rounded-lg p-2 relative ${todo.isDone ? 'border-gray-400/50' : 'border-red-400/40'}`}>
-                        <h2 className='text-xl font-semibold'>
+                    <li key={todo.id} className='flex flex-col border rounded-lg p-2 relative border-gray-700'>
+                        <h2 className='text-xl font-semibold text-sky-300'>
                             {todo.title}
                         </h2>
                         <p>{todo.description}</p>
                         <p>{todo.targetDate}</p>
                         <div className='flex items-center flex-row-reverse absolute top-2 right-2 gap-2'>
-                            <button className={`p-1 ${todo.isDone ? 'text-red-300' : 'text-green-300'}`}>
-                                {todo.isDone ? <CloseIcon className='size-6' /> : <CheckIcon className='size-6' />}
+                            <button
+                                onClick={() => setIsDone(todo.id, !todo.isDone)}
+                                className={todo.isDone ? 'text-green-300 hover:text-green-500 transition-all' : 'text-red-300 hover:text-red-500 transition-all'}
+                            >
+                                {
+                                    todo.isDone ? 'Done' : 'Not Done'
+                                }
                             </button>
                         </div>
                         <div className='flex p-2 gap-5'>
-                            <button className='text-yellow-500 hover:text-yellow-600 transition-all active:scale-110'>
+                            <NavLink to={`/update-todo/${todo.id}`} className='text-yellow-500 hover:text-yellow-600 transition-all active:scale-110'>
                                 <EditIcon />
-                            </button>
-                            <button className='text-red-500 hover:text-red-600 transition-all active:scale-110'>
+                            </NavLink>
+                            <button
+                                onClick={() => removeTodo(todo.id)}
+                                className='text-red-500 hover:text-red-600 transition-all active:scale-110'
+                            >
                                 <TrashIcon />
                             </button>
                         </div>
